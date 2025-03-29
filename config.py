5cd4e7f00029a8281a2495ae8497c1a1d3619a37
Drex-netizen <divyeshkalleda09@gmail.com>
2025-03-29 09:28:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7010380190:AAF6nH1Ip7Ip2ER2tSGE5msGt9ZzpZWWmLg")
-    API_ID = int(os.environ.get("API_ID", 20995706))
-    API_HASH = os.environ.get("API_HASH", "3240c1615daa1fbbca45c34a9bb8ecf2")
-    AUTH_USERS = os.environ.get("AUTH_USERS", "7184539514")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7890079753:AAEA_Dy9VbmUK3KTnrw5baEwirQFloaq04I")
+    API_ID = int(os.environ.get("API_ID", 27886943))
+    API_HASH = os.environ.get("API_HASH", "b28d0dae5a57f767716f1d8865560fee")
+    AUTH_USERS = os.environ.get("AUTH_USERS", "6586191138")
